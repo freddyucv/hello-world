b2dec504c309ed61ebdd57a3e33f3ea11ab4c3a4
Freddy Rodriguez <freddyrodriguez@MacBook-Pro.local>
2016-02-26 09:05:16 -0430

    testing

--- dotcms_109.js
+++ dotcms_109.js
@@ -2,4 +2,5 @@ var gb = document.getElementById(“gb”);
 
 var newlink = document.createElement('a');
 newlink.setAttribute('href', ‘http://localhost:8080/index’);
+newlink. textContent = ‘dotcms’;
 gb.appendChild(newlink);
